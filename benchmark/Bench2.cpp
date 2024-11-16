23f7a54d8ae3100ed2c052e64a730553062bc68e
et-yula <117507873+et-yula@users.noreply.github.com>
2024-11-16 11:19:08 +0300

    fix rand

--- benchmark/Bench2.cpp
+++ benchmark/Bench2.cpp
@@ -1,17 +1,16 @@
 // Copyright 2024 et-yula
 
 #include <algorithm>
-#include <cstdlib>
 #include <iostream>
 #include <vector>
 
 void benchmark_sort(int size, int runs) {
+  unsigned int seed = 123;
   for (int run = 0; run < runs; ++run) {
     std::vector<int> arr(size);
     for (int &num : arr) {
-      num = rand() % 1000000;
+      num = rand_r(&seed) % 1000000;
     }
-
     std::sort(arr.begin(), arr.end());
   }
 }
@@ -21,11 +20,8 @@ int main(int argc, char *argv[]) {
     std::cerr << "Usage: " << argv[0] << " <size> <runs>" << std::endl;
     return 1;
   }
-
   int size = std::atoi(argv[1]);
   int runs = std::atoi(argv[2]);
-
   benchmark_sort(size, runs);
-
   return 0;
 }
