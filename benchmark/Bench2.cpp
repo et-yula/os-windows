faa944bd91896150c7d59d94a2a165642bd072f1
et-yula <117507873+et-yula@users.noreply.github.com>
2024-11-16 11:12:19 +0300

    fix tester 6

--- benchmark/Bench2.cpp
+++ benchmark/Bench2.cpp
@@ -1,27 +1,19 @@
 // Copyright 2024 et-yula
 
 #include <algorithm>
-#include <chrono>
 #include <cstdlib>
 #include <iostream>
-#include <random>
 #include <vector>
 
 void benchmark_sort(int size, int runs) {
-  std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-  std::uniform_int_distribution<int> dist(1, 1000000);
-
-  auto start = std::chrono::high_resolution_clock::now();
   for (int run = 0; run < runs; ++run) {
     std::vector<int> arr(size);
     for (int &num : arr) {
-      num = dist(rng);
+      num = rand() % 1000000;
     }
 
     std::sort(arr.begin(), arr.end());
   }
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double, std::milli> duration = end - start;
 }
 
 int main(int argc, char *argv[]) {
